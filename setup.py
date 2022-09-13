24cef9d20398cbe6481942e341492dbd6bec3749
sean-bailey <seanbailey518@gmail.com>
2022-09-12 22:18:06 -0400

    Checking to see if we can trick pypi to allow github repo installations

--- setup.py
+++ setup.py
@@ -4,24 +4,10 @@ from setuptools import setup, find_packages
 import distutils.text_file
 from pathlib import Path
 from typing import List
-from setuptools.command.develop import develop
-from setuptools.command.install import install
-from subprocess import check_call
+
 
 # Always prefer setuptools over distutils
 import setuptools
-
-class PreDevelopCommand(develop):
-    """Pre-installation for development mode."""
-    def run(self):
-        check_call("pip3 install https://github.com/sean-bailey/Search-Engines-Scraper/archive/refs/heads/master.zip".split())
-        develop.run(self)
-
-class PreInstallCommand(install):
-    """Pre-installation for installation mode."""
-    def run(self):
-        check_call("pip3 install https://github.com/sean-bailey/Search-Engines-Scraper/archive/refs/heads/master.zip".split())
-        install.run(self)
 
 def _parse_requirements(filename: str) -> List[str]:
     """Return requirements from requirements file."""
@@ -30,12 +16,12 @@ def _parse_requirements(filename: str) -> List[str]:
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-PreInstallCommand.run()
+
 print(find_packages())
 
 setup(
     name='newsripper',
-    version='0.1.0a',
+    version='0.1.0',
     packages=find_packages(),
 
     author="Sean Bailey",
